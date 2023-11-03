56e6195817e3f006ace082f5e831a38783bc65fe
shokii NoteBook <shilo152004@mail.ru>
2023-11-03 09:17:23 +0300

    Add 1, 2, 2-2, 3 tasks

---
+++ CLion/Practice_3/BinHash.h
@@ -0,0 +1,43 @@
+#ifndef ABOBA_BINHASH_H
+#define ABOBA_BINHASH_H
+
+#include "Bin.h"
+#include "Hash.h"
+
+class HashBin{
+    Bin bin;
+    HashTable hashTable;
+public:
+    void Linking(string BinaryFilename) {
+        ifstream InputFile(BinaryFilename, ios::binary);
+
+        if (!InputFile) { cout << "Failed to open files\n"; return; }
+
+        BankAccount bank;
+        while (InputFile.read(reinterpret_cast<char*> (&bank), sizeof(BankAccount))){
+            hashTable.Insert(bank);
+        }
+
+        cout << "The bonding was successful!" << endl;
+        InputFile.close();
+    }
+
+    void Deleting(string BinaryFilename, int key){
+        bin.Remove(BinaryFilename, key);
+        hashTable.Remove(key);
+    }
+
+    BankAccount Search(int key){
+        return hashTable.Search(key);
+    }
+
+    void Display(string BinaryFilename){
+        bin.PrintBinary(BinaryFilename);
+        hashTable.PrintHash();
+    }
+
+
+
+};
+
+#endif //ABOBA_BINHASH_H
---
+++ CLion/Practice_3/Hash.h
@@ -0,0 +1,129 @@
+#ifndef ABOBA_HASH_H
+#define ABOBA_HASH_H
+
+#include <iostream>
+#include <vector>
+
+#include "Bin.h"
+
+class HashTable {
+private:
+    static const int BASE_TABLE = 4;
+    std::vector<BankAccount> table;
+    int size = 0;
+
+    int hash1(int accountNumber) { return accountNumber % table.size(); }
+
+    int hash2(int accountNumber) { return (accountNumber % (table.size() - 1)) + 1; }
+
+    void resizeAndRehash() {
+        int newTableSize = table.size() * 2;
+        std::vector<BankAccount> newTable(newTableSize);
+
+        for (const BankAccount& account : table) {
+            if (account.accountNumber != 0) {
+                int index = hash1(account.accountNumber);
+                int step = hash2(account.accountNumber);
+
+                while (newTable[index].accountNumber != 0) {
+                    index = (index + step) % newTableSize;
+                }
+
+                newTable[index] = account;
+            }
+        }
+
+        table = newTable;
+    }
+
+public:
+// Done
+    HashTable(){
+        table = std::vector<BankAccount>(BASE_TABLE);
+    }
+// Done
+    void Insert(BankAccount account) {
+        if(size == table.size()-1){
+            resizeAndRehash();
+        }
+
+        int index = hash1(account.accountNumber);
+        int step = hash2(account.accountNumber);
+
+        while (table[index].accountNumber != 0) {
+            if (table[index].accountNumber == account.accountNumber) {
+                std::cout << "An account with this number already exists. Data update." << std::endl;
+                table[index] = account;
+                return;
+            }
+            index = (index + step) % table.size();
+        }
+
+        table[index] = account;
+        size++;
+    }
+// Done
+    BankAccount Search(int accountNumber) {
+        int index = hash1(accountNumber);
+        int step = hash2(accountNumber);
+
+        while (table[index].accountNumber != 0) {
+            if (table[index].accountNumber == accountNumber) {
+                return table[index];
+            }
+            index = (index + step) % table.size();
+        }
+
+        return BankAccount(0, "", ""); // Счет не найден
+    }
+// Done
+    void Remove(int accountNumber) {
+        int index = hash1(accountNumber);
+        int step = hash2(accountNumber);
+
+        while (table[index].accountNumber != 0) {
+            if (table[index].accountNumber == accountNumber) {
+                table[index] = BankAccount(0, "", ""); // Очищаем ячейку
+                return;
+            }
+            index = (index + step) % table.size();
+        }
+        size--;
+    }
+// Done
+    void PrintHash() {
+        cout << "========================[ Print hash ]========================" << endl;
+        for (int i = 0; i < table.size(); ++i) {
+            if (table[i].accountNumber != 0) {
+                std::cout << "Hash " << i << ": (" << table[i].accountNumber << ", " << table[i].fullName << ", " << table[i].address << ")" << std::endl;
+            }
+        }
+    }
+
+// Сделать HashTest
+    void testHeshT() {
+        HashTable table;
+        BankAccount bankAccount;
+        bankAccount.accountNumber = 1;
+        table.Insert(bankAccount);
+        bankAccount.accountNumber += 1;
+        table.Insert(bankAccount);
+        bankAccount.accountNumber += 1;
+        table.Insert(bankAccount);
+        bankAccount.accountNumber += 1;
+        table.Insert(bankAccount);
+        table.PrintHash();
+        cout << "Add elem" << endl;
+        bankAccount.accountNumber += 1;
+        table.Insert(bankAccount);
+        table.PrintHash();
+        cout << "delete elem with key 4" << endl;
+        table.Remove(4);
+        table.PrintHash();
+        bankAccount = table.Search(3);
+        std::cout << "Hash " << 4 << ": (" << bankAccount.accountNumber << ", " << bankAccount.fullName << ", " << bankAccount.address << ")" << std::endl;
+    }
+
+};
+
+#endif //ABOBA_HASH_H
